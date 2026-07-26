88475bb9d33e072fdffb6ab2089eb16b861b124b
Arya Pahari <aryapahari007@gmail.com>
2026-07-26 18:25:33 -0400

    hi

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Movement.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name="Movement", group="Linear OpMode")
+public class Movement extends LinearOpMode {
+
+    @Override
+    public void runOpMode() {
+        DcMotor leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        DcMotor rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        DcMotor leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        DcMotor rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        //The following code ensures that positive power allows the device to move forward.
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            double y = -this.gamepad1.left_stick_y;
+            double x = this.gamepad1.left_stick_x;
+            double rx = this.gamepad1.right_stick_x;
+            double s = 1; //sensitivity setting for rotation
+
+            double LFPower = y + x + s * rx;
+            double RFPower = y - x - s * rx;
+            double LBPower = y - x + s * rx;
+            double RBPower = y + x - s * rx;
+
+
+
+
+        }
+
+    }
+
+}
