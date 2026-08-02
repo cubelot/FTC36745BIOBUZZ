df8d4f3b6cab4819db167f5c455efe0127a63593
cubelot <tianmu.tim.sha@gmail.com>
2026-08-02 19:14:00 -0400

    another krish

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -11,6 +13,7 @@ import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
@@ -18,7 +21,13 @@ public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
             .mass(6.50)
             .forwardZeroPowerAcceleration(-42.343572953089065)
-            .lateralZeroPowerAcceleration(-57.035110445951176);
+            .lateralZeroPowerAcceleration(-57.035110445951176)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.075,0,0.001, 0.02))
+            .headingPIDFCoefficients(new PIDFCoefficients(0.7, 0, 0.002, 0.032))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.55, 0, 0.0001, 0.6, 0.02))
+            ;
+
+
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
             .rightFrontMotorName("rf")
@@ -31,7 +40,7 @@ public class Constants {
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
             .xVelocity(64.48004390686518)
             .yVelocity(55.82799524021899);
-    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1.5, 1);
     public static PinpointConstants localizerConstants = new PinpointConstants()
             .forwardPodY(-3)
             .strafePodX(3.5)
