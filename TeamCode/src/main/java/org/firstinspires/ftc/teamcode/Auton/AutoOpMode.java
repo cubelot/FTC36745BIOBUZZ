2174b1d8e2f1da386fb69d20c93da2e0ae5ea07d
ks911 <krishnan.raghavendran85@gmail.com>
2026-08-17 09:31:08 -0400

    fixed Constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AutoOpMode.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.Auton;
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-public class AutoOpMode extends LinearOpMode{
-    @Override
-    public void runOpMode() {
-    }
-}
