c1c3b38556b3741a80a22f476ef8109d9b5e4873
ks911 <krishnan.raghavendran85@gmail.com>
2026-09-09 17:50:47 -0400

    added Kickoff Dance (KickoffDance.java)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/KickoffDance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/KickoffDance.java
@@ -2,13 +2,28 @@ package org.firstinspires.ftc.teamcode.Auton;
 
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 public class KickoffDance extends LinearOpMode {
 
     private final Pose startPose = new Pose(72,72,Math.toRadians(90));
-    private final Pose toTheLeft = new Pose (48, 72, Math.toRadians(90));
-    private final Pose toTheRight = new Pose (96,72,Math.toRadians(90));
+    private final Pose toTheLeft_2 = new Pose (48, 72, Math.toRadians(90));
+    private final Pose toTheRight_2 = new Pose (96,72,Math.toRadians(90));
+    private final Pose takeItBack = new Pose(72, 48, Math.toRadians(90));
+
+    private final Pose toTheLeft_1 = new Pose (60,72,Math.toRadians(90));
+    private final Pose toTheRight_1 = new Pose (84, 72, Math.toRadians(90));
+
+    private final Pose hopThisTime_2 = new Pose (72, 84, Math.toRadians(90));
+    private final Pose hopThisTime_1 = new Pose (72, 78, Math.toRadians(90));
+
+    private final Pose crissCross_1 = new Pose (48, 84, Math.toRadians(90));
+    private final Pose crissCross_2 = new Pose (96, 84, Math.toRadians(90));
+
+    private Follower follower;
+
+    Pat
 
     @Override
     public void runOpMode() throws InterruptedException {
