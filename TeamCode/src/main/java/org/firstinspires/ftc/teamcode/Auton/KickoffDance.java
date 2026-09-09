5839a3e2a2ce9fbe3b0763540f271f5ea562b8a7
cubelot <68877583+cubelot@users.noreply.github.com>
2026-09-09 18:38:46 -0400

    asd

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/KickoffDance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/KickoffDance.java
@@ -13,12 +13,11 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
-import java.util.Timer;
 
 @Autonomous(name = "Kickoff Dance")
 public class KickoffDance extends LinearOpMode {
 
-    private final Pose startPose1 = new Pose(72,72,Math.toRadians(90));
+    private final Pose startPose = new Pose(72,72,Math.toRadians(90));
     private final Pose toTheLeft_2 = new Pose (48, 72, Math.toRadians(90));
     private final Pose toTheRight_2 = new Pose (96,72,Math.toRadians(90));
     private final Pose takeItBack = new Pose(72, 48, Math.toRadians(90));
@@ -60,7 +59,7 @@ public class KickoffDance extends LinearOpMode {
         while (opModeIsActive()) {
             follower.update();
 
-
+        }
     }
 
     public void shimmy(Follower follower, int cycles, long speed) {
