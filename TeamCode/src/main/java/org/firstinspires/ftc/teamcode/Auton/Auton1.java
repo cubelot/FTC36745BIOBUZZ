f9636760eacfdccf7116153044b3573a59910521
ks911 <krishnan.raghavendran85@gmail.com>
2026-09-20 19:43:36 -0400

    started auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/Auton1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/Auton1.java
@@ -1,4 +1,31 @@
 package org.firstinspires.ftc.teamcode.Auton;
 
-public class Auton1 {
+import com.pedropathing.api.PoseFactory;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.math.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Auton1 extends OpMode {
+
+    private Follower follower;
+
+    private final PoseFactory p = PoseFactory.degrees();
+
+    private DcMotor outtake = hardwareMap.get(DcMotor.class, "outtake");
+    private DcMotor intake = hardwareMap.get(DcMotor.class, "intake");
+
+    private final Pose startPose = p.of(82, 12, 90);
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
